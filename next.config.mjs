ea22c6575115ecaed30cf29acc605414f824f165
BenjaDreamBox <b.lattanzimerino@gmail.com>
2024-07-22 00:29:35 -0400

    Remove ESLint and fix TypeScript issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 export default {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   reactStrictMode: true,
